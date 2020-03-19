1eb5729cb809f46a3b911fb21e347ad874ebdb80
ShooterGuy666 <p.kling98@hotmail.com>
2020-03-19 13:17:31 +0100

    installatie django
    installatie django

---
+++ Periode_7/Django/Project-website/project-venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Users\Pascal\Documents\P_School_Github\Periode_7\Django\Project-website\project-venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
