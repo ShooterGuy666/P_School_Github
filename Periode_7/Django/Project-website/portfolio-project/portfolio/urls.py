ae206034444fe5b6385d1ccf64aa592cc45d2aae
ShooterGuy666 <p.kling98@hotmail.com>
2020-03-19 15:09:36 +0100

    tussencommit
    19-3-2020 15:09

--- Periode_7/Django/Project-website/portfolio-project/portfolio/urls.py
+++ Periode_7/Django/Project-website/portfolio-project/portfolio/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+import werken.views
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-]
+
+    #path('', views.home, name='home'),
+    path('', werken.views.home, name='home'),
+    path('werken/', include('werken.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
