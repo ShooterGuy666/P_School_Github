fbdb392957f4845bab54c19a451ee693e3a73c1d
ShooterGuy666 <p.kling98@hotmail.com>
2020-03-24 17:53:11 +0100

    opdracht gio toegevoegd

---
+++ Periode_7/Javascript/Opdracht Gio/main.js
@@ -0,0 +1,12 @@
+const text = 'hallo'
+let jaar = 2019
+//Schrijf hier een script waarmee 'jaar' op 2020 komt te staan
+// en als jaar gelijk staat aan 2020 'text' wordt gelogt naar de console 
+if(typeof(text ==='string')){
+    jaar += 1
+}
+
+if(jaar === 2020){
+    console.log(text)
+}
+
