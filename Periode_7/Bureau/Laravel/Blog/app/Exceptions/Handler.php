2165d107533e7064d10a9c95d54599c92bc3ca7d
ShooterGuy666 <p.kling98@hotmail.com>
2020-04-07 14:51:28 +0200

    tot Part 22

--- Periode_7/Bureau/Laravel/Blog/app/Exceptions/Handler.php
+++ Periode_7/Bureau/Laravel/Blog/app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Arr;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -51,5 +53,24 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        $guard = Arr::get($exception->guards(),0);
+
+        switch ($guard) {
+            case 'admin':
+                $redirect = route('admin.home');
+                break;
+            
+            default:
+                $redirect = route('login');
+                break;
+        }
+
+        return $request->expectsJson()
+                    ? response()->json(['message' => $exception->getMessage()], 401)
+                    : redirect()->guest($exception->redirectTo() ?? route('login'));
     }
 }
--- Periode_7/Bureau/Laravel/Blog/app/Http/Controllers/admin/Auth/LoginController.php
+++ Periode_7/Bureau/Laravel/Blog/app/Http/Controllers/admin/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = 'admin.home';
 
     /**
      * Create a new controller instance.
@@ -56,5 +56,10 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function guard()
+    {
+        return Auth::guard('admin');
     }
 }
